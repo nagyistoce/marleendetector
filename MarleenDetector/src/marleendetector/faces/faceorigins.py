39c792351c8e79e75fd28e1c6f1ae5968e8040f5
RTLindeman <RTLindeman@6fb253aa-fc77-11dd-8c4e-4bbb75a408b7>
2009-04-14 17:27:21 +0000

    added further implementation of FaceDB

--- MarleenDetector/src/marleendetector/faces/faceorigins.py
+++ MarleenDetector/src/marleendetector/faces/faceorigins.py
@@ -8,23 +8,81 @@ Created on 31 mrt 2009
 '''
 from pysqlite2 import dbapi2 as sqlite
 
+from marleendetector.gallerymanager import *
+from marleendetector.faces.face import *
+
+
+
 class FaceOriginsDB:
     
     def __init__(self):
-        self.connection = sqlite.connect('faces.db')
-        self.cursor = connection.cursor()
-        # Check  if table exists
-        self.cursor.execute('SELECT name FROM sqlite_master WHERE type="table" AND name="FaceData" ORDER BY name;')
-        tables = cursor.fetchall()
-        if (len(tables)):
-            print "Table 'FaceData' not found, create it!"
-            self.cursor.execute('CREATE TABLE FaceData (id INTEGER PRIMARY KEY, org_image_id VARCHAR, face_image_id INTEGER, ul.x INTEGER, ul.y INTEGER, dr.x INTEGER, dr.y INTEGER)')
+        """
+        Creates a FaceData table
+        """
+        db_location = GALLERY_DATABASE + "\\faces.db"
+        self.connection = sqlite.connect(db_location)
+        self.cursor = self.connection.cursor()
+        # init tables
+        self.__initTables()
 
+    def __initTables(self):
+        """
+            Creates tables when they don't exist
+        """
+        # Check FaceData table
+        self.cursor.execute('SELECT name FROM sqlite_master WHERE type="table" AND name="FaceData" ORDER BY name;')
+        tables = self.cursor.fetchall()
+        if (len(tables) == 0):
+            print "Table 'FaceData' not found, create it!"
+            self.__createFaceDataTable()
+        # Check ImageOrigin table
+        self.cursor.execute('SELECT name FROM sqlite_master WHERE type="table" AND name="ImageOrigin" ORDER BY name;')
+        tables = self.cursor.fetchall()
+        if (len(tables) == 0):
+            print "Table 'ImageOrigin' not found, create it!"
+            self.__createImageOriginTable()
+
+    def __createImageOriginTable(self):
+        """
+            Creates the ImageOrigin table
+        """
+        self.cursor.execute('CREATE TABLE ImageOrigin (id INTEGER PRIMARY KEY, org_image_name VARCHAR, local_image_id VARCHAR)')
+        self.connection.commit()
+            
+    def __createFaceDataTable(self):
+        """
+            Creates the FaceData table
+        """
+        self.cursor.execute('CREATE TABLE FaceData (id INTEGER PRIMARY KEY, local_image_id VARCHAR, face_image_id INTEGER, ul_x INTEGER, ul_y INTEGER, dr_x INTEGER, dr_y INTEGER)')
+        self.connection.commit()
+
+
+
+    def getFaceData(self, local_image_id):
+        """
+            Returns a list of Face-objects
+        """
+        self.cursor.execute('SELECT * FROM FaceData WHERE local_image_id = ? ORDER BY id', (local_image_id,))
+        row = self.cursor.fetchone()
+        face = Face(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+        return face
+
+    def addImageOrigin(self, org_image_name, local_image_id, local_path):
+        values_tuple = (org_image_name, local_image_id, local_path)
+        self.cursor.execute('INSERT INTO ImageOrigin VALUES (NULL, ?, ?, ?)', values_tuple)
+        # Save (commit) the changes
+        self.connection.commit()        
+        
+                
     def addFaceData(self, face_data):
+        """
+        add the face_data to the database
+        face_data = ('BARL_0001', 0, (cvPoint(341,159), cvPoint(449,267)))
+        """
         # values_tuple ('BARL_0001', 0, (cvPoint(341,159), cvPoint(449,267)))
-        (org_image_id, face_image_id, face_rectangle) = face_data
+        (local_image_id, face_image_id, face_rectangle) = face_data
         (ul, dr) = face_rectangle
-        values_tuple = (org_image_id, face_image_id, ul.x, ul.y, dr.x, dr.y)
+        values_tuple = (local_image_id, face_image_id, ul.x, ul.y, dr.x, dr.y)
         self.cursor.execute('INSERT INTO FaceData VALUES (NULL, ?, ?, ?, ?, ?, ?)', values_tuple)
         # Save (commit) the changes
         self.connection.commit()        
